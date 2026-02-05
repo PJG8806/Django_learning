65dec67b70f9fbd362c39e8906133e72b5251150
PJG8806 <jinguy39@gmail.com>
2026-02-05 16:22:20 +0900

    #Commit Title Type ✨ feat : 09day 실습내용(post 기능) #🐛 fix : 기능 수정, 버그 수정 #💡 chore : 오타 수정 및 새로운 기능이 추가되지 않고, 코드가 변경 된 경우 (주석 추가 및 수정 포함) #📝 docs : 문서 수정 (readme 수정 시) #🚚 build : 빌드 관련 파일 수정 및 삭제한 경우 #✅ test:테스트 코드,리팩터링 테스트 코드 추가(프로덕션 코드 변경 X) #♻️ refactor : 코드 리팩터링 #🚑 hotfix : 긴급 수정

---
+++ pystagram/post/forms.py
@@ -0,0 +1,33 @@
+from django.forms import inlineformset_factory
+
+from post.models import  Post, PostImage
+from utils.forms import BootstrapModelForm
+
+
+class PostForm(BootstrapModelForm):
+    class Meta:
+        model = Post
+        fields = ('content', )
+
+
+class PostImageForm(BootstrapModelForm):
+    class Meta:
+        model = PostImage
+        fields = ('image',)
+
+
+PostImageFormSet = inlineformset_factory(
+    Post, PostImage, form=PostImageForm, extra=1, can_delete=True
+)
+# extra는 추가할 자식 폼의 수를 설정, can_delete 폼의 삭제 여부를 설정
+# min_num, max_num 설정은 최소 저장 수와 최대 저장수를 설정하여 최대수를 넘기면 이미지는 들어가지만 저장은 안된다
+# form은 formset의 forms 로 사용이 가능하며 forms에 visible_fields 인풋 박스를 설정하며 지금은 이미지 선택이 출력된다
+# form -> formaset=[PostImageForm(),PostImageForm()] 식으로 만들어짐
+# inlineformset_factory 첫번째는 위에 PostImageForm() 부분에 PostImageForm(post=post) 식으로 넣어준다
+# 부모 모델의 인스턴스를 편집할 때 자식 모델의 인스턴스도 함께 관리할 수 있는 폼셋을 자동으로 생성
+
+# formset = [
+#     PostImageForm(),
+#     PostImageForm(),
+#     PostImageForm(),
+# ]
